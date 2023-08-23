5266e566d418295f9dadbced261d7f2d58fa4f1f
analysed soft <analysedsoft@gmail.com>
2023-08-23 13:19:52 +0000

    Update file service.py

--- service_for_elasticsearch/main/service.py
+++ service_for_elasticsearch/main/service.py
@@ -562,23 +562,31 @@ async def get_list():
     fields = ["doc_id", "page", "project_id", "node_id", "user_id", "type_id", "property_id", "type_name", "property_name", "filename", "created", "path"]
 
     # Define the search query to retrieve all documents
-    query = {"query": {"match_all": {}}}
+    query = {"query": {"match_all": {}}, "size": 10000}
 
-    # Use the search API to retrieve the documents and extract the fields
     try:
         results = es.search(index=INDEX, body=query, _source=fields)
-    except Exception as e: abort(500, str(e))
+    except es_exceptions.BadRequestError as e:
+        error_message = "Bad request: " + str(e)
+        return jsonify({"message": error_message, "status": 400}), 400
+    except es_exceptions.ConnectionError as e:
+        error_message = "Elasticsearch connection error: " + str(e)
+        return jsonify({"message": error_message, "status": 500}), 500
+    except Exception as e:
+        error_message = "An error occurred: " + str(e)
+        return jsonify({"message": error_message, "status": 500}), 500
 
-    # Iterate through the results and extract the fields from each document
     documents = []
     for hit in results["hits"]["hits"]:
-        document = {"filename": hit["_source"]["filename"], "doc_id": hit["_source"]["doc_id"], "type_id" : hit["_source"]["type_id"], "page" : hit["_source"]["page"],\
-                    "created": hit["_source"]["created"], "project_id" : hit["_source"]["project_id"], "node_id" : hit["_source"]["node_id"], "path" : hit["_source"]["path"]}
+        document = {
+            "filename": hit["_source"]["filename"],
+            "doc_id": hit["_source"]["doc_id"],
+            "type_id": hit["_source"]["type_id"],
+            # Include other fields you want to extract
+        }
         documents.append(document)
-        
-    # Print the list of documents
-    
-    return jsonify({'docs' : documents, 'status' : 200})
+
+    return jsonify({"docs": documents, "status": 200})
     
 
 @app.route("/update", methods=["PUT"])
